eb34adb94b307259c21c565bb7fca9e6f010fe55
calexchable <calexchable@gmail.com>
2019-08-08 18:50:08 -0500

    Added MAN to what's up, calex

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("What's up!");
+        System.out.println("What's up, man!");
     }
 }
